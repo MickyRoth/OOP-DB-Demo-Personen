2a6e72d0b4ea88f17967434c435d69ca4ae6356f
Micky Roth <roth@wifa.de>
2025-01-31 10:10:23 +0100

    Mit neuer Ausgabe und mehr Datensätzen

--- Personen-DB-OOP.py
+++ Personen-DB-OOP.py
@@ -8,11 +8,11 @@ class Person:
         self.age = age
 
     def __repr__(self): # Spezial-Methode zum printen des Objektes
-        return f"Person(ID={self.id}, Name='{self.name}', Alter={self.age})"
+        return f"{self.id:2}    {self.name:8} {self.age:2}"
 
 # Verbindung zur SQLite-Datenbank herstellen (Datei-basiert)
 conn = sqlite3.connect("example.db")
-cursor = conn.cursor()
+cursor = conn.cursor()  # SQL-Datenbankcursor-Objekt erzeugen
 
 # Tabelle erstellen
 cursor.execute("""
@@ -23,10 +23,13 @@ CREATE TABLE IF NOT EXISTS persons (
 """)
 
 # Mehrere Datensätze einfügen mit cursor.executemany(liste)
+
+cursor.execute("INSERT INTO persons (name, age) VALUES ('Otto',22)")
+               
 cursor.executemany("INSERT INTO persons (name, age) VALUES (?, ?)",
    [("Alice", 30), ("Bob", 25), ("Charlie", 35)] )
 
-conn.commit()  # Änderungen speichern
+conn.commit()  # Änderungen speichern (Transaktionsbestätigung)
 
 # Datensätze per SQL abrufen
 cursor.execute("SELECT * FROM persons")
@@ -35,14 +38,14 @@ daten = cursor.fetchall() # Ergebnis der Abfrage in Variable daten kopieren
 conn.close() # Nicht vergessen die DB-Verbindung zu schließen
 
 # Datensätz in Objekte einlesen
-persons = []
-for row in daten:
+persons = []      # Leere Liste für die Objekte
+for row in daten: # Mit for durch das Ergebnis der Abfrage
     id = row[0]   # Erstes Datenfeld
     name = row[1] # Zweites Datenfeld
     age = row[2]  # Drittes Datenfeld
-    persons.append(Person(id,name,age))  # Person-Objekt erzeugen
+    persons.append(Person(id,name,age))  # Person-Objekt erzeugen und an Liste anhängen
 
 # Objekte ausgeben
-for person in persons:
-    print(person)  # Dank der Methode __repr__() klappt das Objekt-printen
+for row in persons:
+    print(row)  # Dank der Methode __repr__() klappt das Objekt-printen
 
